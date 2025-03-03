e7c45e702654f32edca785c03cee9dc378e1087f
xen-42 <nicholasconnors@hotmail.ca>
2025-03-02 21:48:52 -0500

    Add fabric template

---
+++ src/client/java/xen42/peacefulitems/PeacefulItemsClient.java
@@ -0,0 +1,10 @@
+package xen42.peacefulitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeacefulItemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
