f5276b3312ec70b022c42e2d2222d094d5814f65
xen-42 <nicholasconnors@hotmail.ca>
2025-03-02 22:42:29 -0500

    Copy over textures and add guano, batwings, and sulphur

--- src/main/java/xen42/peacefulitems/PeacefulItems.java
+++ src/main/java/xen42/peacefulitems/PeacefulItems.java
@@ -1,24 +1,43 @@
 package xen42.peacefulitems;
 
-import net.fabricmc.api.ModInitializer;
+import java.util.function.Function;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
-public class PeacefulItems implements ModInitializer {
-	public static final String MOD_ID = "peaceful-items";
+public class PeacefulItems {
+    public static final Item BAT_WING = register("bat_wing", Item::new, new Item.Settings());
+    public static final Item GUANO = register("guano", Item::new, new Item.Settings());
+    public static final Item SULPHUR = register("sulphur", Item::new, new Item.Settings());
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static void initialize() {
+        // Add custom items to groups
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> { 
+            itemGroup.add(BAT_WING);
+            itemGroup.add(GUANO);
+            itemGroup.add(SULPHUR);
+        });
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        CompostingChanceRegistry.INSTANCE.add(GUANO, 1f);
+    }
 
-		LOGGER.info("Hello Fabric world!");
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+		// Create the item key.
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(PeacefulItemsMod.MOD_ID, name));
+
+		// Create the item instance.
+		Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item.
+		Registry.register(Registries.ITEM, itemKey, item);
+
+		return item;
 	}
 }
