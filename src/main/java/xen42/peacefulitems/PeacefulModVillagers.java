f3800430576ecb1ecd77bf1a779e98b9ab46c014
xen-42 <nicholasconnors@hotmail.ca>
2025-03-16 16:18:45 -0400

    Make musician villager sell horns

--- src/main/java/xen42/peacefulitems/PeacefulModVillagers.java
+++ src/main/java/xen42/peacefulitems/PeacefulModVillagers.java
@@ -10,13 +10,16 @@ import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.GoatHornItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.InstrumentTags;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.SoundEvent;
@@ -99,7 +102,7 @@ public class PeacefulModVillagers {
             factories.add((entity, random) -> new TradeOffer(
 				new TradedItem(Items.EMERALD, 2),
 				new ItemStack(Blocks.NOTE_BLOCK, 1), 12, 5, 0.05f));
-            factories.add((entity, random) -> RandomDisc(entity, random));
+            factories.add((entity, random) -> RandomHorn(entity, random));
 		});
 
         TradeOfferHelper.registerVillagerOffers(DJ_VILLAGER, 3, factories -> {
@@ -117,7 +120,7 @@ public class PeacefulModVillagers {
 		});
 
         TradeOfferHelper.registerVillagerOffers(DJ_VILLAGER, 5, factories -> {
-            factories.add((entity, random) -> RandomDisc(entity, random));
+            factories.add((entity, random) -> RandomHorn(entity, random));
             factories.add((entity, random) -> RandomDisc(entity, random));
 		});
     }
@@ -126,6 +129,16 @@ public class PeacefulModVillagers {
         var item = entity.getWorld().getRegistryManager().getOrThrow(RegistryKeys.ITEM).getRandomEntry(ItemTags.CREEPER_DROP_MUSIC_DISCS, random).get();
         return new TradeOffer(
             new TradedItem(Items.EMERALD, 24),
-            new ItemStack(item, 1), 8, 30, 0.05f);
+            new ItemStack(item, 1), 1, 30, 0.05f);
     }
+
+	private static TradeOffer RandomHorn(Entity entity, Random random) {
+		var tag = random.nextBoolean() ? InstrumentTags.SCREAMING_GOAT_HORNS : InstrumentTags.REGULAR_GOAT_HORNS;
+		var horn = entity.getWorld().getRegistryManager().getOrThrow(RegistryKeys.INSTRUMENT).getRandomEntry(tag, random)
+			.map(registryEntry -> GoatHornItem.getStackForInstrument(Items.GOAT_HORN, registryEntry))
+			.orElseGet(() -> new ItemStack(Items.GOAT_HORN));
+		return new TradeOffer(
+			new TradedItem(Items.EMERALD, 6),
+			horn, 2, 5, 0.05f);
+	}
 }
