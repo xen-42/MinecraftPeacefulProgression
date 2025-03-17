fcf968b16fed07a09b23d9da1cc8002b47bd8b34
xen-42 <nicholasconnors@hotmail.ca>
2025-03-16 20:57:08 -0400

    This pains me to no end

--- src/main/java/xen42/peacefulitems/entities/GhastlingEntity.java
+++ src/main/java/xen42/peacefulitems/entities/GhastlingEntity.java
@@ -1,22 +1,20 @@
 package xen42.peacefulitems.entities;
 
+import java.util.logging.LogManager;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.Flutterer;
-import net.minecraft.entity.MovementType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.control.FlightMoveControl;
 import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.ai.goal.EscapeDangerGoal;
 import net.minecraft.entity.ai.goal.FlyGoal;
-import net.minecraft.entity.ai.goal.FollowMobGoal;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.TemptGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
-import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
@@ -32,7 +30,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
@@ -43,16 +41,16 @@ public class GhastlingEntity extends AnimalEntity implements Flutterer {
 
     public GhastlingEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
-        //this.moveControl = new FlightMoveControl(this, 20, true);
+        this.moveControl = new GhastlingMoveControl(this);
     }
 
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.2f));
-        this.goalSelector.add(2, new TemptGoal(this, 1.2f, Ingredient.ofItem(PeacefulModItems.SULPHUR), false));
-        this.goalSelector.add(3, new WanderAroundFarGoal(this, 1f));
-        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 1f));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 2f));
+        this.goalSelector.add(2, new TemptGoal(this, 1.5f, Ingredient.ofItem(PeacefulModItems.SULPHUR), false));
+        this.goalSelector.add(3, new FlyGoal(this, 1f));
+        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 3f));
         this.goalSelector.add(5, new LookAroundGoal(this));
     }
 
@@ -114,8 +112,8 @@ public class GhastlingEntity extends AnimalEntity implements Flutterer {
     public static DefaultAttributeContainer.Builder createMobAttributes() {
         return AnimalEntity.createAnimalAttributes()
             .add(EntityAttributes.MAX_HEALTH, 10)
-            .add(EntityAttributes.FLYING_SPEED, 0.3f)
-            .add(EntityAttributes.MOVEMENT_SPEED, 0.2f)
+            .add(EntityAttributes.FLYING_SPEED, 0.6f)
+            .add(EntityAttributes.MOVEMENT_SPEED, 0.3f)
             .add(EntityAttributes.FALL_DAMAGE_MULTIPLIER, 0f)
             .add(EntityAttributes.TEMPT_RANGE, 20f);
     }
@@ -130,27 +128,6 @@ public class GhastlingEntity extends AnimalEntity implements Flutterer {
         return !isOnGround();
     }
 
-    /*
-    @Override
-    public void travel(Vec3d movementInput) {
-        if (isLogicalSideForUpdatingMovement()) {
-            if (isTouchingWater()) {
-                updateVelocity(0.02f, movementInput);
-                move(MovementType.SELF, getVelocity());
-                setVelocity(getVelocity().multiply(0.8f));
-            } else if (isInLava()) {
-                updateVelocity(0.02f, movementInput);
-                move(MovementType.SELF, getVelocity());
-                setVelocity(getVelocity().multiply(0.5f));
-            } else {
-                updateVelocity(getMovementSpeed(), movementInput);
-                move(MovementType.SELF, getVelocity());
-                setVelocity(getVelocity().multiply(0.9f));
-            } 
-        }
-    }
-    */
-
     @Override
     protected void fall(double heightDifference, boolean onGround, BlockState state, BlockPos landedPosition) {}
 
@@ -162,5 +139,45 @@ public class GhastlingEntity extends AnimalEntity implements Flutterer {
     @Override
     public boolean isFlappingWings() {
         return !isOnGround();
+    }
+}
+
+class GhastlingMoveControl extends MoveControl {
+
+    public GhastlingMoveControl(MobEntity entity) {
+        super(entity);
+    }
+
+    @Override
+    public void tick() {
+        PeacefulMod.LOGGER.info("STATE:" + state);
+
+        if (this.state == MoveControl.State.MOVE_TO) {
+			this.state = MoveControl.State.WAIT;
+			double dx = this.targetX - this.entity.getX();
+			double dy = this.targetZ - this.entity.getZ();
+			double dz = this.targetY - this.entity.getY();
+			double distSquared = dx * dx + dz * dz + dy * dy;
+			if (distSquared < 2.5E-7F) {
+				this.entity.setForwardSpeed(0.0F);
+                this.entity.setUpwardSpeed(0.0F);
+				return;
+			}
+
+			float yaw = (float)(MathHelper.atan2(dy, dx) * 180.0F / (float)Math.PI) - 90.0F;
+			this.entity.setYaw(this.wrapDegrees(this.entity.getYaw(), yaw, 90.0F));
+            var speed = (float)(this.speed * this.entity.getAttributeValue(entity.isOnGround() ? EntityAttributes.MOVEMENT_SPEED : EntityAttributes.FLYING_SPEED));
+			this.entity.setMovementSpeed(speed);		
+            
+            if (Math.abs(dy) > 1e-5) {
+                this.entity.setUpwardSpeed(dy > 0.0 ? speed : -speed);
+            }
+        }
+        else {
+            this.entity.setForwardSpeed(0.0F);
+            this.entity.setUpwardSpeed(0.0F);
+        }
+        this.state = MoveControl.State.WAIT;
+
     }
 }
