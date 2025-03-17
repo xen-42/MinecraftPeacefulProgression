3ec319fbfcb1579c63c062fa63196e37023b6d5d
xen-42 <nicholasconnors@hotmail.ca>
2025-03-17 00:40:39 -0400

    Add the other nether star recipes and totem of undying recipe

--- src/client/java/xen42/peacefulitems/PeacefulModRecipeGenerator.java
+++ src/client/java/xen42/peacefulitems/PeacefulModRecipeGenerator.java
@@ -2,17 +2,24 @@ package xen42.peacefulitems;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.util.Identifier;
 
 public class PeacefulModRecipeGenerator extends FabricRecipeProvider {
 	public PeacefulModRecipeGenerator(FabricDataOutput generator, CompletableFuture<WrapperLookup> registriesFuture) {
@@ -86,19 +93,24 @@ public class PeacefulModRecipeGenerator extends FabricRecipeProvider {
                         .criterion(hasItem(PeacefulModBlocks.BLAZE_PICKLE), conditionsFromItem(PeacefulModBlocks.BLAZE_PICKLE))
                         .offerTo(exporter);
 
-                // TODO: add the thing with groups for the different froglights
-                createShaped(RecipeCategory.MISC, Items.NETHER_STAR)
-                        .pattern(" b ")
-                        .pattern("bab")
-                        .pattern("ddd")
-                        .input('a', Blocks.OCHRE_FROGLIGHT)
-                        .input('b', Items.GHAST_TEAR)
-                        .input('d', Items.DIAMOND)
-                        // Advancement that gives the recipe
-                        .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
-                        .criterion(hasItem(Items.GHAST_TEAR), conditionsFromItem(Items.GHAST_TEAR))
-                        .criterion(hasItem(Blocks.OCHRE_FROGLIGHT), conditionsFromItem(Blocks.OCHRE_FROGLIGHT))
-                        .offerTo(exporter);
+                BiConsumer<String, Block> makeNetherStarRecipe = (id, frogLight) -> {
+                        createShaped(RecipeCategory.MISC, Items.NETHER_STAR)
+                                .group("peaceful-items.nether_star")
+                                .pattern(" b ")
+                                .pattern("bab")
+                                .pattern("ddd")
+                                .input('a', frogLight)
+                                .input('b', Items.GHAST_TEAR)
+                                .input('d', Items.DIAMOND)
+                                // Advancement that gives the recipe
+                                .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
+                                .criterion(hasItem(Items.GHAST_TEAR), conditionsFromItem(Items.GHAST_TEAR))
+                                .criterion(hasItem(frogLight), conditionsFromItem(frogLight))
+                                .offerTo(exporter, RegistryKey.of(RegistryKeys.RECIPE, Identifier.of(PeacefulMod.MOD_ID, id)));
+                };
+                makeNetherStarRecipe.accept("ochre_nether_star", Blocks.OCHRE_FROGLIGHT);
+                makeNetherStarRecipe.accept("verdant_nether_star", Blocks.VERDANT_FROGLIGHT);
+                makeNetherStarRecipe.accept("pearlescent_nether_star", Blocks.PEARLESCENT_FROGLIGHT);
 
                 createShapeless(RecipeCategory.MISC, Items.PHANTOM_MEMBRANE, 2)
                         .input(PeacefulModItems.BAT_WING, 3) 
