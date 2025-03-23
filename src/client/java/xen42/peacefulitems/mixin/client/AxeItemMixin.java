f785e49bcc9a5893062b8ba8635f7c01ea9fabc8
xen-42 <nicholasconnors@hotmail.ca>
2025-03-23 02:10:06 -0400

    Add in resin from stripping pale oak logs

---
+++ src/client/java/xen42/peacefulitems/mixin/client/AxeItemMixin.java
@@ -0,0 +1,39 @@
+package xen42.peacefulitems.mixin.client;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.World;
+
+@Mixin(AxeItem.class)
+public class AxeItemMixin {
+    @Inject(at = @At("HEAD"), method = "useOnBlock")
+    public void useOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> info) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        PlayerEntity playerEntity = context.getPlayer();
+        var state = world.getBlockState(blockPos);
+        if (state.isOf(Blocks.PALE_OAK_LOG) && !world.isClient()) {
+            var offset = playerEntity.getPos().subtract(blockPos.toCenterPos()).normalize();
+            var dropPos = offset.add(blockPos.toCenterPos());
+            var roundedPos = new Vec3i((int)dropPos.getX(), (int)dropPos.getY(), (int)dropPos.getZ());
+            var itemEntity = new ItemEntity(world, dropPos.getX(), dropPos.getY(), dropPos.getZ(), new ItemStack(Items.RESIN_CLUMP));
+            world.spawnEntity(itemEntity);
+        }
+    }
+}
