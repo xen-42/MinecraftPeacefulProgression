cdcb8bf2db19e2943d06b84b28b5c3626768860f
xen-42 <nicholasconnors@hotmail.ca>
2025-05-14 22:17:25 -0400

    Fix sulphur cluster not breaking when the block under it breaks

--- src/main/java/xen42/peacefulitems/blocks/SulphurClusterBlock.java
+++ src/main/java/xen42/peacefulitems/blocks/SulphurClusterBlock.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.Nullable;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.fluid.FluidState;
@@ -19,10 +20,12 @@ import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 import xen42.peacefulitems.PeacefulModItems;
 
 public class SulphurClusterBlock extends Block implements Waterloggable {
@@ -112,4 +115,12 @@ public class SulphurClusterBlock extends Block implements Waterloggable {
     protected ItemStack getPickStack(WorldView world, BlockPos pos, BlockState state, boolean includeData) {
         return new ItemStack(PeacefulModItems.SULPHUR);
     }
+
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        if ((Boolean)state.get(WATERLOGGED)) {
+            tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return direction == ((Direction)state.get(FACING)).getOpposite() && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+   }
 }
