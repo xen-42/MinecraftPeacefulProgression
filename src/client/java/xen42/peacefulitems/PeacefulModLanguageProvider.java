57ed2d5143050463323d4dafa970eb03c28685d6
xen-42 <nicholasconnors@hotmail.ca>
2025-05-23 00:14:56 -0400

    Change hunger gamerules to be enable peaceful healing and enabling peaceful starvation

--- src/client/java/xen42/peacefulitems/PeacefulModLanguageProvider.java
+++ src/client/java/xen42/peacefulitems/PeacefulModLanguageProvider.java
@@ -129,7 +129,8 @@ public abstract class PeacefulModLanguageProvider extends FabricLanguageProvider
 			translationBuilder.addFilledMap(PeacefulModTags.StructureTags.TRAIL_RUINS, "Trail Ruins Map");
 			translationBuilder.addFilledMap(StructureTags.OCEAN_RUIN, "Ocean Ruin Map");
 			
-			translationBuilder.add(PeacefulMod.DISABLE_HUNGER_PEACEFUL, "Disable hunger in peaceful");
+			translationBuilder.add(PeacefulMod.ENABLE_STARVING_PEACEFUL, "Enable starving in peaceful");
+			translationBuilder.add(PeacefulMod.ENABLE_SUPER_HEALING_PEACEFUL, "Enable super healing in peaceful");
 			translationBuilder.add(PeacefulMod.DISABLE_ENDER_DRAGON_FIGHT_PEACEFUL, "Disable Ender Dragon fight in peaceful");
 
 			translationBuilder.add(PeacefulModTags.ItemTags.EFFIGIES, "Effigies");
@@ -234,7 +235,7 @@ public abstract class PeacefulModLanguageProvider extends FabricLanguageProvider
 			translationBuilder.addFilledMap(PeacefulModTags.StructureTags.TRAIL_RUINS, "古迹废墟地图");
 			translationBuilder.addFilledMap(StructureTags.OCEAN_RUIN, "海底废墟地图");
 			
-			translationBuilder.add(PeacefulMod.DISABLE_HUNGER_PEACEFUL, "禁用和平中的饥饿");
+			//translationBuilder.add(PeacefulMod.DISABLE_HUNGER_PEACEFUL, "禁用和平中的饥饿");
 			translationBuilder.add(PeacefulMod.DISABLE_ENDER_DRAGON_FIGHT_PEACEFUL, "禁用和平中的末影龙");
 
 			translationBuilder.add(PeacefulModTags.ItemTags.EFFIGIES, "塑像");
--- src/main/java/xen42/peacefulitems/PeacefulMod.java
+++ src/main/java/xen42/peacefulitems/PeacefulMod.java
@@ -93,10 +93,12 @@ public class PeacefulMod implements ModInitializer {
 	public static int BatGrowUpTicks = 5 * 60 * 20; // Normal mobs its 20 minutes but I feel like bats can grow up fast maybe idk!
 	public static int BatBreedingCooldown = 5 * 60 * 20;
 
-	public static final GameRules.Key<BooleanRule> DISABLE_HUNGER_PEACEFUL =
-		GameRuleRegistry.register("disableHungerPeaceful", Category.PLAYER, GameRuleFactory.createBooleanRule(false));
 	public static final GameRules.Key<BooleanRule> DISABLE_ENDER_DRAGON_FIGHT_PEACEFUL =
 		GameRuleRegistry.register("disableEnderDragonFightPeaceful", Category.MOBS, GameRuleFactory.createBooleanRule(true));
+	public static final GameRules.Key<BooleanRule> ENABLE_SUPER_HEALING_PEACEFUL =
+		GameRuleRegistry.register("enableSuperHealingPeaceful", Category.PLAYER, GameRuleFactory.createBooleanRule(true));
+	public static final GameRules.Key<BooleanRule> ENABLE_STARVING_PEACEFUL =
+		GameRuleRegistry.register("enableStarvingPeaceful", Category.PLAYER, GameRuleFactory.createBooleanRule(false));
 
 	public static final RegistryKey<EntityType<?>> GHASTLING_ENTITY_KEY = RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(MOD_ID,"ghastling"));
 	public static final EntityType<GhastlingEntity> GHASTLING_ENTITY = Registry.register(
