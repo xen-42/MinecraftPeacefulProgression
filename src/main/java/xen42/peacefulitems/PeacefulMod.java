024a5edf24d4b5426a6e2f26e5b8e4d2d3ed96fc
xen-42 <22628069+xen-42@users.noreply.github.com>
2025-12-06 17:32:55 -0500

    Force more enderclams to spawn

--- src/main/java/xen42/peacefulitems/PeacefulMod.java
+++ src/main/java/xen42/peacefulitems/PeacefulMod.java
@@ -155,7 +155,8 @@ public class PeacefulMod implements ModInitializer {
 		BiomeModifications.addSpawn(ghastlingBiomes, SpawnGroup.AMBIENT, GHASTLING_ENTITY, 100, 2, 3);
 		SpawnRestriction.register(GHASTLING_ENTITY, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, GhastlingEntity::isValidSpawn);
 
-		BiomeModifications.addSpawn(clamBiomes, SpawnGroup.AMBIENT, END_CLAM_ENTITY, 100, 1, 1);
+		// Clams don't spawn enough in the Nether so on top of this we do some extra stuff with CustomSpawners
+		BiomeModifications.addSpawn(clamBiomes, SpawnGroup.AMBIENT, END_CLAM_ENTITY, 100, 2, 5);
 		SpawnRestriction.register(END_CLAM_ENTITY, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, EndClamEntity::isValidSpawn);
 
 		PayloadTypeRegistry.playS2C().register(EffigyParticlePayload.ID, EffigyParticlePayload.CODEC);
---
+++ src/main/java/xen42/peacefulitems/mixin/MinecraftServerMixin.java
@@ -0,0 +1,34 @@
+package xen42.peacefulitems.mixin;
+
+import java.util.ArrayList;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.entity.EntityType;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.spawner.Spawner;
+import xen42.peacefulitems.CustomSpawner;
+import xen42.peacefulitems.PeacefulMod;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    // This is what makes the nether also superflat
+	@Inject(at = @At("RETURN"), method = "createWorlds")
+    private void createWorlds(WorldGenerationProgressListener listener, CallbackInfo info) {
+        MinecraftServer server = (MinecraftServer) (Object) this;
+
+        ServerWorld nether = server.getWorld(World.NETHER);
+        var spawners = new ArrayList<Spawner>();
+        spawners.addAll(nether.spawners);
+        spawners.add(new CustomSpawner(PeacefulMod.END_CLAM_ENTITY).setBiome(BiomeSelectors.includeByKey(BiomeKeys.WARPED_FOREST)).setMaxCount(10)); 
+        nether.spawners = spawners;
+    }
+}
