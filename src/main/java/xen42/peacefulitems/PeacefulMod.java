adbcb5b6ed823a0ad7fb2bb16d66eabb6ed6d79d
xen-42 <22628069+xen-42@users.noreply.github.com>
2025-12-06 13:12:30 -0500

    Add an event when using effigy totems

--- src/main/java/xen42/peacefulitems/PeacefulMod.java
+++ src/main/java/xen42/peacefulitems/PeacefulMod.java
@@ -140,6 +140,7 @@ public class PeacefulMod implements ModInitializer {
 
 		LOGGER.info("Loading Peaceful Mod!");
 
+		PeacefulModEvents.onInitialize();
 		PeacefulModItems.initialize();
 		PeacefulModBlocks.initialize();
 		PeacefulModFluids.initialize();
--- src/main/java/xen42/peacefulitems/PeacefulModItems.java
+++ src/main/java/xen42/peacefulitems/PeacefulModItems.java
@@ -116,6 +116,7 @@ public class PeacefulModItems {
     public static final Item WITHER_EFFIGY = register("wither_effigy", (settings) -> 
         new EffigyItem(settings, "wither_effigy", (ServerPlayerEntity user) -> {
             user.dropItem(user.getServerWorld(), Items.NETHER_STAR);
+            PeacefulModEvents.WITHER_TOTEM_USE_EVENT.invoker().onUse(user);
         }, SoundEvents.ENTITY_WITHER_DEATH),
         new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
 
@@ -133,6 +134,8 @@ public class PeacefulModItems {
             user.dropStack(world, new ItemStack(Items.PRISMARINE_CRYSTALS, user.getRandom().nextBetween(2, 12)));
             user.dropStack(world, new ItemStack(Items.PRISMARINE_SHARD, user.getRandom().nextBetween(2, 8)));
 
+            PeacefulModEvents.GUARDIAN_TOTEM_USE_EVENT.invoker().onUse(user);
+
         }, SoundEvents.ENTITY_ELDER_GUARDIAN_CURSE),
         new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
     
@@ -141,6 +144,8 @@ public class PeacefulModItems {
             ServerWorld end = user.getServer().getWorld(World.END);
             var fight = end.getEnderDragonFight();
             ((EnderDragonFight_Invoker)fight).invokeGenerateNewEndGateway();
+
+            PeacefulModEvents.DRAGON_TOTEM_USE_EVENT.invoker().onUse(user);
         }, SoundEvents.ENTITY_ENDER_DRAGON_DEATH), new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
 
     public static final Item RAID_EFFIGY = register("raid_effigy", (settings) -> 
@@ -176,6 +181,8 @@ public class PeacefulModItems {
             user.dropStack(world, new ItemStack(Items.SUGAR, user.getRandom().nextBetween(1, 12)));
             user.dropStack(world, new ItemStack(Items.SPIDER_EYE, user.getRandom().nextBetween(1, 12)));
             user.dropStack(world, new ItemStack(Items.GLASS_BOTTLE, user.getRandom().nextBetween(1, 12)));
+
+            PeacefulModEvents.RAID_TOTEM_USE_EVENT.invoker().onUse(user);
         }, SoundEvents.EVENT_RAID_HORN), new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
 
     public static final Item CLAM = register("clam_meat", Item::new, 
