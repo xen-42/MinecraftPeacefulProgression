fab570f74dd6eb979b2c0fb4c7cdee2e1d32f8c0
Cassian <cassian@godsted.com>
2026-01-10 18:25:58 -0500

    Use new client entrypoint

--- src/client/java/xen42/peacefulitems/rrv/PeacefulProgressionClientRRVPlugin.java
+++ src/client/java/xen42/peacefulitems/rrv/PeacefulProgressionClientRRVPlugin.java
@@ -1,11 +1,13 @@
 package xen42.peacefulitems.rrv;
 
+import cc.cassian.rrv.api.ReliableRecipeViewerClientPlugin;
 import cc.cassian.rrv.api.ReliableRecipeViewerPlugin;
 import cc.cassian.rrv.api.recipe.ItemView;
+import cc.cassian.rrv.client.ReliableRecipeViewerClient;
 
 import java.util.Collections;
 
-public class PeacefulProgressionClientRRVPlugin implements ReliableRecipeViewerPlugin {
+public class PeacefulProgressionClientRRVPlugin implements ReliableRecipeViewerClientPlugin {
 
     @Override
     public void onIntegrationInitialize() {
