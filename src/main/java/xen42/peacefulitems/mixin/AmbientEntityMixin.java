bb46a94e4923e88d5935431623c4f8736747a762
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-03 01:54:50 -0400

    apparently I missed this but got it later

--- src/main/java/xen42/peacefulitems/mixin/AmbientEntityMixin.java
+++ src/main/java/xen42/peacefulitems/mixin/AmbientEntityMixin.java
@@ -7,12 +7,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.mob.AmbientEntity;
 import net.minecraft.entity.passive.BatEntity;
+import net.minecraft.entity.player.PlayerEntity;
 
 @Mixin(AmbientEntity.class)
 public class AmbientEntityMixin {
     // Has to be done here because this method actually defines the method whereas BatEntity doesnt change it
-    @Inject(at = @At("RETURN"), method = "canBeLeashed", cancellable = true)
-    public void canBeLeashedBy(CallbackInfoReturnable<Boolean> info) {
+    @Inject(at = @At("RETURN"), method = "canBeLeashedBy", cancellable = true)
+    public void canBeLeashedBy(PlayerEntity player, CallbackInfoReturnable<Boolean> info) {
 		var entity = ((AmbientEntity)(Object)this);
         if (entity instanceof BatEntity) {
             info.setReturnValue(!entity.isLeashed());
