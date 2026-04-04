652cb73f086f893063264a5b9158606d8cdb79db
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-04 06:17:56 -0400

    Can not open doors

--- src/main/java/xen42/peacefulitems/entities/GhastlingEntity.java
+++ src/main/java/xen42/peacefulitems/entities/GhastlingEntity.java
@@ -162,7 +162,7 @@ public class GhastlingEntity extends AnimalEntity implements Flutterer {
                 return !this.world.getBlockState(pos.down()).isAir();
             }
         };
-        navigation.setCanPathThroughDoors(true);
+        navigation.setCanPathThroughDoors(false);
         navigation.setCanSwim(true);
         navigation.setMaxFollowRange(48f);
         return navigation;
