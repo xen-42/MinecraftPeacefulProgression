818d0d9858baaef5ca44234c067fba4422645575
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-03 22:26:17 -0400

    Make a little better

--- src/main/java/xen42/peacefulitems/PeacefulModEvents.java
+++ src/main/java/xen42/peacefulitems/PeacefulModEvents.java
@@ -1,5 +1,6 @@
 package xen42.peacefulitems;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -10,45 +11,54 @@ import net.minecraft.server.network.ServerPlayerEntity;
 // Events used for mod compatibility 
 // Probably terrible, should work with reflection (just call RegisterListener)
 public class PeacefulModEvents {
+    @FunctionalInterface
     public interface TotemEventCallback {
         public void onUse(ServerPlayerEntity user);
     }
 
-    private static Event<TotemEventCallback> createTotemEvent() {
-        return EventFactory.createArrayBacked(TotemEventCallback.class, listeners -> (user) -> {
+    private static final Map<String, Event<TotemEventCallback>> EVENTS = new HashMap<>();
+
+    public static final Event<TotemEventCallback> DRAGON_TOTEM_USE_EVENT =
+        createTotemEvent("DRAGON_TOTEM_USE_EVENT");
+
+    public static final Event<TotemEventCallback> WITHER_TOTEM_USE_EVENT =
+        createTotemEvent("WITHER_TOTEM_USE_EVENT");
+
+    public static final Event<TotemEventCallback> RAID_TOTEM_USE_EVENT =
+        createTotemEvent("RAID_TOTEM_USE_EVENT");
+
+    public static final Event<TotemEventCallback> GUARDIAN_TOTEM_USE_EVENT =
+        createTotemEvent("GUARDIAN_TOTEM_USE_EVENT");
+
+    private static Event<TotemEventCallback> createTotemEvent(String id) {
+        var event = EventFactory.createArrayBacked(TotemEventCallback.class, listeners -> user -> {
+            PeacefulMod.LOGGER.info("Firing totem event '{}' for {}", id, user.getName().getString());
+
             for (var listener : listeners) {
                 listener.onUse(user);
             }
         });
+
+        EVENTS.put(id, event);
+        return event;
     }
 
     public static void RegisterTotemEventListener(String eventName, Consumer<ServerPlayerEntity> listener) {
-        var callback = new TotemEventCallback() {
-            @Override
-            public void onUse(ServerPlayerEntity user) {
-                listener.accept(user);
-            }
-        };
-        var event = map.get(eventName);
-        if (event != null) {
-            event.register(callback);
+        var event = EVENTS.get(eventName);
+
+        if (event == null) {
+            PeacefulMod.LOGGER.error(
+                "Unknown totem event '{}'. Possible version mismatch? Valid events: {}",
+                eventName,
+                EVENTS.keySet()
+            );
+            return;
         }
-        else {
-            PeacefulMod.LOGGER.error("Could not find event named " + eventName + ". Possible version mismatch?");
-        }
+
+        event.register(listener::accept);
+
+        PeacefulMod.LOGGER.info("Registered listener for '{}'", eventName);
     }
-
-    public static final Event<TotemEventCallback> DRAGON_TOTEM_USE_EVENT = createTotemEvent();
-    public static final Event<TotemEventCallback> WITHER_TOTEM_USE_EVENT = createTotemEvent();
-    public static final Event<TotemEventCallback> RAID_TOTEM_USE_EVENT = createTotemEvent();
-    public static final Event<TotemEventCallback> GUARDIAN_TOTEM_USE_EVENT = createTotemEvent();
-
-    private static Map<String, Event<TotemEventCallback>> map = Map.of(
-        "DRAGON_TOTEM_USE_EVENT", DRAGON_TOTEM_USE_EVENT,
-        "WITHER_TOTEM_USE_EVENT", WITHER_TOTEM_USE_EVENT,
-        "RAID_TOTEM_USE_EVENT", RAID_TOTEM_USE_EVENT,
-        "GUARDIAN_TOTEM_USE_EVENT", GUARDIAN_TOTEM_USE_EVENT
-    );
 
     public static void onInitialize() { }
 }
