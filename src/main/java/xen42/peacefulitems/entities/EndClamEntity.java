455e0478abf5bb884dc3ccae3ce25b69482f06b1
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-05 04:37:41 -0400

    Jade plugin

--- src/main/java/xen42/peacefulitems/entities/EndClamEntity.java
+++ src/main/java/xen42/peacefulitems/entities/EndClamEntity.java
@@ -247,10 +247,18 @@ public class EndClamEntity extends AmbientEntity {
         return successfulTeleport;    
     } 
 
+    public ItemStack getContainedItem() {
+    	return getEquippedStack(EquipmentSlot.MAINHAND);
+    }
+    
+    public void setContainedItem(ItemStack stack) {
+    	equipStack(EquipmentSlot.MAINHAND, stack);
+    }
+
     @Override
     public void tickMovement() {
         super.tickMovement();
-        if ((getWorld()).isClient && getEquippedStack(EquipmentSlot.MAINHAND).isOf(Items.ENDER_PEARL)) {
+        if ((getWorld()).isClient && getContainedItem().isOf(Items.ENDER_PEARL)) {
             getWorld().addParticle((ParticleEffect)ParticleTypes.PORTAL, 
                 getParticleX(0.25D), 
                 getRandomBodyY() + 0.25D, 
@@ -265,10 +273,10 @@ public class EndClamEntity extends AmbientEntity {
     protected void loot(ItemEntity itemEntity) {
         ItemStack itemStack = itemEntity.getStack();
         if (!this.getDataTracker().get(IS_OPENING)) {
-            if (!getEquippedStack(EquipmentSlot.MAINHAND).isEmpty()) {
+            if (!getContainedItem().isEmpty()) {
                 if (!getWorld().isClient) {
                     var thrownItem = new ItemEntity(getWorld(), getX() + (getRotationVector()).x, getY() + 1.0D, getZ() + (getRotationVector()).z, 
-                        getEquippedStack(EquipmentSlot.MAINHAND));
+                        getContainedItem());
                     thrownItem.setPickupDelay(40);
                     thrownItem.setThrower(this);
                     getWorld().spawnEntity(thrownItem);
@@ -281,7 +289,7 @@ public class EndClamEntity extends AmbientEntity {
             }
 
             triggerItemPickedUpByEntityCriteria(itemEntity);
-            equipStack(EquipmentSlot.MAINHAND, itemStack.split(1));
+            setContainedItem(itemStack.split(1));
             updateDropChances(EquipmentSlot.MAINHAND);
             sendPickup(itemEntity, itemStack.getCount());
             itemEntity.discard();
@@ -300,10 +308,10 @@ public class EndClamEntity extends AmbientEntity {
 
     @Override
     protected void drop(DamageSource damageSource) {
-        var itemStack = getEquippedStack(EquipmentSlot.MAINHAND);
+        var itemStack = getContainedItem();
         if (!itemStack.isEmpty()) {
             dropStack(itemStack);
-            equipStack(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+            setContainedItem(ItemStack.EMPTY);
         }
         super.drop(damageSource);
     }
@@ -324,7 +332,7 @@ public class EndClamEntity extends AmbientEntity {
                 item = Items.GOLD_NUGGET;
             }
 
-            equipStack(EquipmentSlot.MAINHAND, new ItemStack(item));
+            setContainedItem(new ItemStack(item));
         }
         // Face a random direction
         setYaw(this.random.nextFloat() * 360f);
