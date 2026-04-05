c148242fec28dde87f666e2e172751cb6126496e
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-05 10:00:45 -0400

    hostile music disc tag

--- src/main/java/xen42/peacefulitems/PeacefulModVillagers.java
+++ src/main/java/xen42/peacefulitems/PeacefulModVillagers.java
@@ -172,7 +172,7 @@ public class PeacefulModVillagers {
     }
 
     private static TradeOffer RandomDisc(ServerWorld world, Entity entity, Random random) {
-        var item = entity.getEntityWorld().getRegistryManager().getOrThrow(RegistryKeys.ITEM).getRandomEntry(ItemTags.CREEPER_DROP_MUSIC_DISCS, random).get();
+        var item = entity.getEntityWorld().getRegistryManager().getOrThrow(RegistryKeys.ITEM).getRandomEntry(PeacefulModTags.ItemTags.HOSTILE_MUSIC_DISCS, random).get();
         return new TradeOffer(
             new TradedItem(Items.EMERALD, 24),
             new ItemStack(item, 1), 1, 30, 0.05f);
