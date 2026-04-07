9525d9ec250b3c4f550b7317d89ab79588188e19
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-04-07 00:42:15 -0400

    Move sounds to separate class

--- src/main/java/xen42/peacefulitems/PeacefulMod.java
+++ src/main/java/xen42/peacefulitems/PeacefulMod.java
@@ -127,11 +127,6 @@ public class PeacefulMod implements ModInitializer {
 		Identifier.of(MOD_ID, "effigy_altar"),
 		new ScreenHandlerType<EffigyAltarScreenHandler>(EffigyAltarScreenHandler::new, null));
 
-	public static final SoundEvent ITEM_BOTTLE_EMPTY_DRAGONBREATH = Registry.register(
-		Registries.SOUND_EVENT,
-		Identifier.of(MOD_ID, "item.bottle.empty_dragonbreath"),
-		SoundEvent.of(Identifier.of(MOD_ID, "item.bottle.empty_dragonbreath")));
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -141,6 +136,7 @@ public class PeacefulMod implements ModInitializer {
 		LOGGER.info("Loading Peaceful Mod!");
 
 		PeacefulModEvents.onInitialize();
+		PeacefulModSounds.initialize();
 		PeacefulModItems.initialize();
 		PeacefulModBlocks.initialize();
 		PeacefulModFluids.initialize();
