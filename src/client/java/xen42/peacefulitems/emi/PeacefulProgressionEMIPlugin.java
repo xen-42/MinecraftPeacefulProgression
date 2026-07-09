15564045d5b3f17c389fa2c852a53ce0efe8f84b
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-07-09 04:44:28 -0400

    add rei info to emi as well

--- src/client/java/xen42/peacefulitems/emi/PeacefulProgressionEMIPlugin.java
+++ src/client/java/xen42/peacefulitems/emi/PeacefulProgressionEMIPlugin.java
@@ -9,6 +9,7 @@ import java.util.function.Supplier;
 import dev.emi.emi.EmiPort;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiInfoRecipe;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.recipe.EmiRecipeSorting;
@@ -19,14 +20,21 @@ import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.recipe.EmiAnvilRecipe;
 import dev.emi.emi.runtime.EmiDrawContext;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EntityType;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.RecipeManager;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.village.raid.Raid;
 import xen42.peacefulitems.PeacefulMod;
 import xen42.peacefulitems.PeacefulModBlocks;
 import xen42.peacefulitems.PeacefulModFluids;
@@ -82,6 +90,8 @@ public class PeacefulProgressionEMIPlugin implements EmiPlugin {
 		addDragonBreathCauldronRecipes(registry);
 
 		addAnvilRepairRecipe(registry, PeacefulModItems.CAPE, PeacefulModItems.BAT_WING);
+
+		addInformationRecipes(registry);
 	}
 
 	private static EmiIngredient getPreferredTag(TagKey<Item> candidate, EmiIngredient fallback) {
@@ -108,7 +118,9 @@ public class PeacefulProgressionEMIPlugin implements EmiPlugin {
 	}
 
 	private static Identifier synthetic(String type, String name) {
-		return Identifier.of("emi", "/" + type + "/" + name);
+		String path = "/" + type + "/" + name;
+		//PeacefulMod.LOGGER.info(path);
+		return Identifier.of(PeacefulMod.MOD_ID, path);
 	}
 
 	private static Identifier syntheticWorldId(Item item) {
@@ -230,5 +242,162 @@ public class PeacefulProgressionEMIPlugin implements EmiPlugin {
 			cauldron,
 			fluidThird
 		));
+	}
+
+	private static void addInformationRecipes(EmiRegistry registry) {
+		PeacefulMod.LOGGER.info("Registering EMI information");
+		DynamicRegistryManager registryManager = MinecraftClient.getInstance().world.getRegistryManager();
+
+		// Effigy Altar
+		addInfo(registry, PeacefulModBlocks.EFFIGY_ALTAR.asItem(), "dungeon");
+
+		// Effigy
+		Text dragonEffigy = Text.translatable(Items.DRAGON_BREATH.getTranslationKey() + ".dragon_effigy_information");
+		addInfo(registry, Items.DRAGON_BREATH, dragonEffigy);
+		addInfo(registry, PeacefulModFluids.DRAGON_BREATH, dragonEffigy);
+		addInfo(registry, Items.NETHER_STAR, "wither_effigy");
+
+		// Elder Effigy
+		Text guardianEffigy = Text.translatable(PeacefulModItems.GUARDIAN_EFFIGY.getTranslationKey() + ".drop_information");
+		addInfo(registry, Items.COOKED_COD, guardianEffigy);
+		addInfo(registry, Items.PRISMARINE_CRYSTALS, guardianEffigy);
+		addInfo(registry, Items.PRISMARINE_SHARD, guardianEffigy);
+
+		// Raid Effigy
+		Text raidEffigy = Text.translatable(PeacefulModItems.RAID_EFFIGY.getTranslationKey() + ".drop_information");
+		addInfo(registry, Raid.getOminousBanner().getItem(), raidEffigy);
+		addInfo(registry, Items.CROSSBOW, raidEffigy);
+		addInfo(registry, Items.SADDLE, raidEffigy);
+		addInfo(registry, Items.IRON_AXE, raidEffigy);
+		addInfo(registry, Items.EMERALD, raidEffigy);
+		addInfo(registry, Blocks.REDSTONE_WIRE.asItem(), raidEffigy);
+		addInfo(registry, Items.GUNPOWDER, raidEffigy);
+		addInfo(registry, Items.STICK, raidEffigy);
+		addInfo(registry, Items.GLOWSTONE_DUST, raidEffigy);
+		addInfo(registry, Items.SUGAR, raidEffigy);
+		addInfo(registry, Items.SPIDER_EYE, raidEffigy);
+		addInfo(registry, Items.GLASS_BOTTLE, raidEffigy);
+
+		// Enderclam
+		Text clam = Text.translatable(PeacefulMod.END_CLAM_ENTITY.getTranslationKey() + ".drop_information");
+		addInfo(registry, Items.ENDER_PEARL, clam);
+		addInfo(registry, PeacefulModItems.SULPHUR, clam);
+		addInfo(registry, Items.GOLD_NUGGET, clam);
+
+		// Panda
+		addInfo(registry, Items.SLIME_BALL, "sneeze");
+
+		// Frog
+		Text froglight = Text.translatable(Items.MAGMA_CREAM.getTranslationKey() + ".frog_information");
+		addInfo(registry, Items.MAGMA_CREAM, froglight);
+		addInfo(registry, Blocks.OCHRE_FROGLIGHT.asItem(), froglight);
+		addInfo(registry, Blocks.PEARLESCENT_FROGLIGHT.asItem(), froglight);
+		addInfo(registry, Blocks.VERDANT_FROGLIGHT.asItem(), froglight);
+
+		// Wisp
+		addInfo(registry, Items.GHAST_TEAR, "wisp");
+
+		// Bat
+		addInfo(registry, PeacefulModItems.GUANO, "bat");
+
+		// Sniffer
+		Text soulSand = Text.translatable(Blocks.SOUL_SAND.getTranslationKey() + ".sniffer_information");
+		addInfo(registry, Blocks.SOUL_SAND.asItem(), soulSand);
+		addInfo(registry, Blocks.SOUL_SOIL.asItem(), soulSand);
+		addInfo(registry, Items.WITHER_SKELETON_SKULL, soulSand);
+		addInfo(registry, Items.SKELETON_SKULL, soulSand);
+		addInfo(registry, Items.WITHER_ROSE, soulSand);
+		addInfo(registry, PeacefulModBlocks.BLAZE_PICKLE.asItem(), soulSand);
+		addInfo(registry, Items.BONE, soulSand);
+
+		Text sand = Text.translatable(Blocks.SAND.getTranslationKey() + ".sniffer_information");
+		addInfo(registry, Blocks.SAND.asItem(), sand);
+		addInfo(registry, Items.NAUTILUS_SHELL, sand);
+		addInfo(registry, Items.PRISMARINE_SHARD, sand);
+		addInfo(registry, Items.PRISMARINE_CRYSTALS, sand);
+
+		Text gravel = Text.translatable(Blocks.GRAVEL.getTranslationKey() + ".sniffer_information");
+		addInfo(registry, Blocks.GRAVEL.asItem(), gravel);
+		addInfo(registry, Items.FLINT, gravel);
+
+		// Fossil
+		Text brush = Text.translatable(PeacefulModBlocks.FOSSIL_ORE.getTranslationKey() + ".brush_information");
+		addInfo(registry, Items.BRUSH, brush);
+		addInfo(registry, PeacefulModBlocks.FOSSIL_ORE.asItem(), brush);
+		addInfo(registry, PeacefulModBlocks.DEEPSLATE_FOSSIL_ORE.asItem(), brush);
+		addInfo(registry, PeacefulModBlocks.SOUL_SOIL_FOSSIL_ORE.asItem(), brush);
+
+		// Wandering Trader
+		Text wanderingTraderKey = Text.translatable(EntityType.WANDERING_TRADER.getTranslationKey() + ".key_head_information");
+		addInfo(registry, Items.ZOMBIE_HEAD, wanderingTraderKey);
+		addInfo(registry, Items.CREEPER_HEAD, wanderingTraderKey);
+		addInfo(registry, Items.PIGLIN_HEAD, wanderingTraderKey);
+
+		// Dungeon Chest
+		Text headKey = Text.translatable(Items.CREEPER_HEAD.getTranslationKey() + ".dungeon_chest_information");
+		addInfo(registry, Items.CREEPER_HEAD, headKey);
+		addInfo(registry, Items.PIGLIN_HEAD, headKey);
+		addInfo(registry, Items.ZOMBIE_HEAD, headKey);
+	}
+
+	private static void addInfo(EmiRegistry registry, Item item, String prefix) {
+		addInfo(
+			registry,
+			item,
+			Text.translatable(item.getTranslationKey() + "." + prefix + "_information"),
+			prefix
+		);
+	}
+
+	private static void addInfo(EmiRegistry registry, Item item, Text information) {
+		addInfo(registry, item, information, translationKey(information));
+	}
+
+	private static void addInfo(EmiRegistry registry, Item item, Text information, String suffix) {
+		addRecipeSafe(registry, () -> new EmiInfoRecipe(
+			List.of(stack(item)),
+			List.of(information),
+			synthetic("info", subId(item) + "/" + cleanInfoSuffix(item, suffix))
+		));
+	}
+
+	private static void addInfo(EmiRegistry registry, Fluid fluid, Text information) {
+		Identifier id = fluid.getRegistryEntry().registryKey().getValue();
+
+		addRecipeSafe(registry, () -> new EmiInfoRecipe(
+			List.of(stack(fluid, BUCKET)),
+			List.of(information),
+			synthetic("info", id.getNamespace() + "/" + id.getPath() + "/" + cleanIdPart(translationKey(information)))
+		));
+	}
+
+	private static String translationKey(Text text) {
+		if (text.getContent() instanceof net.minecraft.text.TranslatableTextContent translatable) {
+			return translatable.getKey();
+		}
+		return "info";
+	}
+
+	private static String cleanInfoSuffix(Item item, String suffix) {
+		String itemId = subId(item);
+		String cleaned = cleanIdPart(suffix);
+
+		if (cleaned.startsWith(itemId + "/")) {
+			return cleaned.substring(itemId.length() + 1);
+		}
+
+		return cleaned;
+	}
+
+	private static String cleanIdPart(String value) {
+		return value
+			.replace(':', '/')
+			.replace('.', '/')
+			.replace("/item/", "/")
+			.replace("/block/", "/")
+			.replace("/entity/", "/")
+			.replace("item/", "")
+			.replace("block/", "")
+			.replace("entity/", "");
 	}
 }
